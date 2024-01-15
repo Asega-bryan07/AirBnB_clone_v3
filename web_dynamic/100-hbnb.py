87e95bf64a82d7b0b45aa97f7ad6eb174960a87e
Asega-bryan07 <Almasibryan7@gmail.com>
2024-01-16 00:45:48 +0300

    AirBnB Web Dynamic

---
+++ web_dynamic/100-hbnb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+import uuid
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Removes the current SQLAlchemy  Session """
+    storage.close()
+
+
+@app.route('/100-hbnb/', strict_slashes=False)
+def hbnb():
+    ''' The HBNB Runs Now! '''
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    state_city = []
+
+    for state in states:
+        state_city.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+        ammenities = storage.all(Amenity).values()
+        amenities = sorted(amenities, key=lambda k: k.name)
+
+        places = storage.all(Place).values()
+        places = sorted(places, key=lambda k: k.name)
+
+        return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=uuid.uuid4())
+
+
+if __name__ == "__main__":
+app.run(host='0.0.0.0',port=5001)
